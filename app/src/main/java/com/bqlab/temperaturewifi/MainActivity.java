d053a86e78872a75d40b48196d93a7af43c1f410
Choi Sinho <beaqon@naver.com>
2018-08-24 02:05:45 +0900

    테스트가 필요하다

--- app/src/main/java/com/bqlab/temperaturewifi/MainActivity.java
+++ app/src/main/java/com/bqlab/temperaturewifi/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class Room {
-        private String ip;
         private String name;
         private Button view;
         private Socket socket;
@@ -56,40 +55,6 @@ public class MainActivity extends AppCompatActivity {
                     setRoom();
                 }
             });
-        }
-
-        private boolean checkSetIP(String ip) {
-            for (int i = 0; i < ip.length(); i++)
-                if (!"0123456789.".contains(String.valueOf(ip.charAt(i)))) return false;
-            return !ip.isEmpty() && ip.contains(".");
-        }
-
-        private void setIP() {
-            final EditText e = new EditText(MainActivity.this);
-            AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
-            b.setMessage("IP를 입력하세요.");
-            b.setView(e);
-            b.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    if (checkSetIP(e.getText().toString())) {
-                        Toast.makeText(MainActivity.this, e.getText().toString() + "에 연결합니다.", Toast.LENGTH_SHORT).show();
-                        new Thread(new ThreadConnector(e.getText().toString(), 8090)).start();
-                        Room.this.ip = e.getText().toString();
-                        dialogInterface.dismiss();
-                    } else {
-                        Toast.makeText(MainActivity.this, "입력을 다시 확인하세요.", Toast.LENGTH_SHORT).show();
-                        setIP();
-                    }
-                }
-            });
-            b.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();
-                }
-            });
-            b.show();
         }
 
         private void setRoom() {
@@ -120,6 +85,39 @@ public class MainActivity extends AppCompatActivity {
             b.show();
         }
 
+        private void setIP() {
+            final EditText e = new EditText(MainActivity.this);
+            AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+            b.setMessage("IP를 입력하세요.");
+            b.setView(e);
+            b.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    if (checkSetIP(e.getText().toString())) {
+                        Toast.makeText(MainActivity.this, e.getText().toString() + "에 연결합니다.", Toast.LENGTH_SHORT).show();
+                        new Thread(new ThreadConnector(e.getText().toString(), 8090)).start();
+                        dialogInterface.dismiss();
+                    } else {
+                        Toast.makeText(MainActivity.this, "입력을 다시 확인하세요.", Toast.LENGTH_SHORT).show();
+                        setIP();
+                    }
+                }
+            });
+            b.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            b.show();
+        }
+
+        private boolean checkSetIP(String ip) {
+            for (int i = 0; i < ip.length(); i++)
+                if (!"0123456789.".contains(String.valueOf(ip.charAt(i)))) return false;
+            return !ip.isEmpty() && ip.contains(".");
+        }
+
         private class ThreadConnector implements Runnable {
             private String ip;
             private int port;
@@ -132,7 +130,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 String TAG = "tcp";
-
                 try {
                     socket = new Socket(ip, port);
                     ThreadConnector.this.ip = socket.getRemoteSocketAddress().toString();
@@ -141,15 +138,15 @@ public class MainActivity extends AppCompatActivity {
                 } catch (SocketTimeoutException e) {
                     Log.d(TAG, "연결 시간이 초과되었습니다.");
                 } catch (Exception e) {
-                    Log.e(TAG, ("에러가 발생했습니다."));
+                    Log.e(TAG, ("오류가 발생했습니다."));
                 }
+
                 if (socket != null) {
                     try {
                         reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
                         isConnected = true;
                     } catch (IOException e) {
-                        Toast.makeText(MainActivity.this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
-                        finishAffinity();
+                        Log.e(TAG, ("오류가 발생했습니다."));
                     }
                 }
 
